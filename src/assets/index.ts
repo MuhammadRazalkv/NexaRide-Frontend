b7707e31447e6f3ca85d610d7fb23d3a93c36d3d
MuhammadRazalkv <rasalkv21@gmail.com>
2025-07-14 10:51:49 +0530

    asset names changed

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
-export { default as Landing1 } from './user/landing1.jpg';
-export { default as Landing2 } from './user/landing2.jpg';
-export { default as Landing3 } from './user/landing3.jpg';
+export { default as Landing1 } from './user/Landing1.jpg';
+export { default as Landing2 } from './user/Landing2.jpg';
+export { default as Landing3 } from './user/Landing3.jpg';
 export { default as driverPopUp } from './driver/carPopup.png';
 export { default as PopUpDriver } from './driver/DriverPopUp.png';
 export { default as Car3D } from './user/Car3Dmodel.png';
