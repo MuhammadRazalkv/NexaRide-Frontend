96a5b09ca97428414eb6c6bf3c21712ac180923a
MuhammadRazalkv <rasalkv21@gmail.com>
2025-07-14 10:46:02 +0530

    asset folder updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,7 @@
-export { default as Landing1 } from './User assets/landing1.jpg';
-export { default as Landing2 } from './User assets/landing2.jpg';
-export { default as Landing3 } from './User assets/landing3.jpg';
-export { default as driverPopUp } from './DriverAssets/carPopup.png';
-export { default as PopUpDriver } from './DriverAssets/DriverPopUp.png';
-export { default as Car3D } from './User assets/Car3Dmodel.png';
-
-
-export {default as Default_Pfp } from './User assets/Default_pfp.svg'
+export { default as Landing1 } from './user/landing1.jpg';
+export { default as Landing2 } from './user/landing2.jpg';
+export { default as Landing3 } from './user/landing3.jpg';
+export { default as driverPopUp } from './driver/carPopup.png';
+export { default as PopUpDriver } from './driver/DriverPopUp.png';
+export { default as Car3D } from './user/Car3Dmodel.png';
+export {default as Default_Pfp } from './user/Default_pfp.svg'
